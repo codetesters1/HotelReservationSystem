0751554f9556ce51a9863f14fcbd98fcca2f1e07
Waqas Bajwa <wbajwa313@gmail.com>
2018-09-23 20:51:52 +1000

    RoomTest.java 02
    Change in RoomTest for Unit Testing

--- src/hotel/entities/RoomTest.java
+++ src/hotel/entities/RoomTest.java
@@ -1,60 +1,86 @@
+package hotel.entities;
+
+import hotel.credit.CreditCard;
+import hotel.credit.CreditCardType;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class RoomTest {
-	
-	private Room room;
 
-	/*Book method*/
-	public void testBook() {
-	room = new Room(1, RoomType.SINGLE);
-	Guest guest = new Guest("Tom", "Billabong", 0415207775);
-	Date date = new Date();
-	CreditCard creditCard = CreditCard(CreditCardType.MASTERCARD, 123456789, 123);
-	room.book(guest, date, 5, 4, creditCard);
-	List<Booking> bookings = room.getBooking();
-	
-	//Check bookings size
-	assert bookings.size() == 1;
-	
-	//Check booking status
-	Booking booking = bookings.get(0);
-	assert booking.isPending();
-	
-	//isAvailable
-	assert room.isAvailable(date, 5) == false;
-	}
-	
-	
-	/* Chechkin method */
-	public void testCheckin(){
-	room.checkin();
-	
-	//Check room state
-	assert room.getState() == Room.State.OCCUPIED;
-	
-	//Chechk booking state
-	Booking booking = room.getBookings().get(room.getBookings().size()-1);
-    assert booking.isCheckedIn() == true;
-	}
-	
-	/*Checkout method*/
-	public void testCheckOut(){
-	Booking booking = room.getBookings().get(room.getBookings().size()-1);
-    room.checkout(booking);
-	
-	//Check room state
-	assert room.isReady() == true;
-	
-	//Chechk booking state
-	assert booking.isCheckedOut() == true;
-	}
-	
-	public static void main(String[] args){
-		RoomTest roomTest = new RoomTest();
-        roomTest.testBook();
-        roomTest.testCheckin();
-        roomTest.testCheckout();
-	}
-	
+    private Room room;
+    private Date date;
+
+    private void bookRoom() {
+        room = new Room(1, RoomType.SINGLE);
+        Guest guest = new Guest("Tom", "Billabong", 0415207775);
+        date = new Date();
+        CreditCard creditCard = new CreditCard(CreditCardType.MASTERCARD, 123456789, 123);
+        room.book(guest, date, 5, 4, creditCard);
+    }
+
+    @Before
+    public void setup() {
+        bookRoom();
+    }
+
+    /*Book method*/
+    @Test
+    public void testBook() {
+
+        List<Booking> bookings = room.getBookings();
+
+        //Check bookings size
+        assertEquals(1, bookings.size());
+
+
+        //Check booking status
+        Booking booking = bookings.get(0);
+        assertTrue(booking.isPending());
+
+
+        //isAvailable
+        assertFalse(room.isAvailable(date, 5));
+    }
+
+
+    /* Chechkin method */
+    @Test
+    public void testCheckin() {
+        room.checkin();
+
+        //Check room state
+        assertEquals(Room.State.OCCUPIED, room.getState());
+
+        //Chechk booking state
+        Booking booking = room.getBookings().get(room.getBookings().size() - 1);
+        assertTrue(booking.isCheckedIn());
+    }
+
+    /*Checkout method*/
+    @Test
+    public void testCheckOut() {
+        Booking booking = room.getBookings().get(room.getBookings().size() - 1);
+        room.checkin();
+        room.checkout(booking);
+
+        //Check room state
+        assertTrue(room.isReady());
+
+
+        //Chechk booking state
+        assertTrue(booking.isCheckedOut());
+
+    }
+
+
+
 }
 
 
