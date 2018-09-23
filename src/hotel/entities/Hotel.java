b83fba0b1f4b4146bdf212d8fd3fa4749e510164
codetesters1 <41274893+codetesters1@users.noreply.github.com>
2018-09-23 16:12:08 +1000

    Update Hotel.java
    Implemented method checkout

--- src/hotel/entities/Hotel.java
+++ src/hotel/entities/Hotel.java
@@ -136,17 +136,18 @@ public class Hotel {
             }
 	}
 
-	
-	// implementation - HIJAS (21/09/2018)
+	//vidath
 	public void checkout(int roomId) {
-		
-            Booking booking = findActiveBookingByRoomId(roomId);
-            if(booking != null){
+		    //Finding booking by room id 
+            Booking booking = findActiveBookingByRoomId(roomId); 
+            //if booking result is null
+            if(booking == null){
+                //throws a RuntimeException 
+                throw new RuntimeException("Booking is not available.");
+                
+            }else{
                 booking.checkOut();
                 activeBookingsByRoomId.remove(roomId);
-            }else{
-                throw new RuntimeException("No any bookings for this roomId");
-                        
             }
 	}
 
